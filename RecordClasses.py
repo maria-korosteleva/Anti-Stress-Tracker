b8d11779ed1312926e055644b05ce015e25731e0
mariako <mariako@kaist.ac.kr>
2018-06-08 21:24:16 +0900

    front fixes example

--- RecordClasses.py
+++ RecordClasses.py
@@ -1,7 +1,10 @@
 # holds the data of one record of something
 class Record:
     def __init__(self, value, timestamp, sleep=False):
-        self.value = value
+        if value:
+            self.value = value
+        else:
+            self.value = None
         self.datetime = timestamp
         self.sleep = sleep  # true if current record correspond to sleep
 
